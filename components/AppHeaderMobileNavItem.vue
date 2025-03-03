63832499cc98a03ccae9266832957e441f9c54c7
Igor Babko <i.i.babko@gmail.com>
2025-03-03 07:01:44 +0200

    AppHeaderMobileNavItem

---
+++ components/AppHeaderMobileNavItem.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+withDefaults(defineProps<{ linkClasses?: string }>(), {
+  linkClasses:
+    'inline-block w-full rounded-md p-2 text-center text-sm font-semibold hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700',
+})
+</script>
+
+<template>
+  <li class="w-full">
+    <a href="#" :class="linkClasses">
+      <slot />
+    </a>
+  </li>
+</template>
