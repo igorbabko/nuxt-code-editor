9ea388441f791da90a1c8a3924f5990c9eeaf411
Igor Babko <i.i.babko@gmail.com>
2025-03-19 10:56:37 +0200

    playlists page components

---
+++ components/PlaylistFilters.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="flex flex-col gap-y-4 md:col-span-1 lg:gap-y-6">
+    <input
+      type="search"
+      placeholder="Playlist Title"
+      class="rounded-lg border border-gray-300 px-4 py-2 shadow-sm focus:border-indigo-600 focus:ring-1 dark:border-0 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:ring-indigo-400"
+    />
+    <div class="flex flex-col gap-y-2">
+      <h3 class="font-semibold sm:text-lg dark:text-white">Tags</h3>
+      <ul class="flex flex-col gap-y-1">
+        <li>
+          <button
+            class="w-full rounded-md px-3 py-2 text-left text-sm text-gray-600 hover:bg-gray-100 sm:rounded-lg dark:text-gray-300 dark:hover:bg-gray-700"
+          >
+            Productivity
+          </button>
+        </li>
+        <li>
+          <button
+            class="w-full rounded-md px-3 py-2 text-left text-sm text-gray-600 hover:bg-gray-100 sm:rounded-lg dark:text-gray-300 dark:hover:bg-gray-700"
+          >
+            Development
+          </button>
+        </li>
+        <li>
+          <button
+            class="w-full rounded-md px-3 py-2 text-left text-sm text-gray-600 hover:bg-gray-100 sm:rounded-lg dark:text-gray-300 dark:hover:bg-gray-700"
+          >
+            Design
+          </button>
+        </li>
+        <li>
+          <button
+            class="w-full rounded-md px-3 py-2 text-left text-sm text-gray-600 hover:bg-gray-100 sm:rounded-lg dark:text-gray-300 dark:hover:bg-gray-700"
+          >
+            Utility
+          </button>
+        </li>
+        <li>
+          <button
+            class="w-full rounded-md px-3 py-2 text-left text-sm text-gray-600 hover:bg-gray-100 sm:rounded-lg dark:text-gray-300 dark:hover:bg-gray-700"
+          >
+            Integration
+          </button>
+        </li>
+        <li>
+          <button
+            class="w-full rounded-md px-3 py-2 text-left text-sm text-gray-600 hover:bg-gray-100 sm:rounded-lg dark:text-gray-300 dark:hover:bg-gray-700"
+          >
+            Testing
+          </button>
+        </li>
+        <li>
+          <button
+            class="w-full rounded-md px-3 py-2 text-left text-sm text-gray-600 hover:bg-gray-100 sm:rounded-lg dark:text-gray-300 dark:hover:bg-gray-700"
+          >
+            Debugging
+          </button>
+        </li>
+        <li>
+          <button
+            class="w-full rounded-md px-3 py-2 text-left text-sm text-gray-600 hover:bg-gray-100 sm:rounded-lg dark:text-gray-300 dark:hover:bg-gray-700"
+          >
+            Formatting
+          </button>
+        </li>
+        <li>
+          <button
+            class="w-full rounded-md px-3 py-2 text-left text-sm text-gray-600 hover:bg-gray-100 sm:rounded-lg dark:text-gray-300 dark:hover:bg-gray-700"
+          >
+            Collaboration
+          </button>
+        </li>
+        <li>
+          <button
+            class="w-full rounded-md px-3 py-2 text-left text-sm text-gray-600 hover:bg-gray-100 sm:rounded-lg dark:text-gray-300 dark:hover:bg-gray-700"
+          >
+            Documentation
+          </button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
