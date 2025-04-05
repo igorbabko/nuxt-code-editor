174c59c91f5b21f08c4c40c4590a2c8ffe519935
Igor Babko <i.i.babko@gmail.com>
2025-04-05 09:05:35 +0300

    auth

--- pages/register.vue
+++ pages/register.vue
@@ -4,20 +4,12 @@ definePageMeta({ layout: 'auth' })
 
 <template>
   <section class="flex w-full flex-col items-center gap-y-4 p-3 sm:gap-y-6">
-    <a href="#" class="flex items-center gap-x-2">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="currentColor"
+    <NuxtLink to="/" class="flex items-center">
+      <Icon
+        name="code"
         class="size-14 -rotate-6 text-indigo-600 sm:size-16 dark:text-white"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M3 6a3 3 0 0 1 3-3h12a3 3 0 0 1 3 3v12a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3V6Zm14.25 6a.75.75 0 0 1-.22.53l-2.25 2.25a.75.75 0 1 1-1.06-1.06L15.44 12l-1.72-1.72a.75.75 0 1 1 1.06-1.06l2.25 2.25c.141.14.22.331.22.53Zm-10.28-.53a.75.75 0 0 0 0 1.06l2.25 2.25a.75.75 0 1 0 1.06-1.06L8.56 12l1.72-1.72a.75.75 0 1 0-1.06-1.06l-2.25 2.25Z"
-          clip-rule="evenodd"
-        />
-      </svg>
-    </a>
+      />
+    </NuxtLink>
     <div
       class="w-full rounded-md border border-gray-200 bg-white p-4 shadow-sm sm:w-96 sm:rounded-lg sm:p-6 dark:border-black dark:bg-gray-900 dark:shadow-none"
     >
@@ -69,12 +61,12 @@ definePageMeta({ layout: 'auth' })
         class="mt-6 text-center text-sm text-gray-600 sm:mt-8 dark:text-gray-300"
       >
         Already have an account?
-        <a
-          href="#"
+        <NuxtLink
+          to="/login"
           class="font-medium text-indigo-600 hover:text-indigo-700 dark:text-indigo-400 dark:hover:text-indigo-300"
         >
           Log In
-        </a>
+        </NuxtLink>
       </p>
     </div>
   </section>
