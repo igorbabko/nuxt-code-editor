f448d914498c1f867cc0c388d9cb3016075ab304
Igor Babko <i.i.babko@gmail.com>
2025-04-09 10:23:18 +0300

    --- app form field

--- components/AppNewsletterContent.vue
+++ components/AppNewsletterContent.vue
@@ -5,7 +5,7 @@
         type="email"
         placeholder="Your Email"
         id="email"
-        class="w-96"
+        class="w-60 sm:w-96"
       />
       <AppButton>
         <span class="hidden sm:inline">Subscribe</span>
