d6f64d53c023057daa1a918d5d2993c1c81ae217
Igor Babko <i.i.babko@gmail.com>
2025-04-09 09:19:27 +0300

    --- app form field

--- components/AppNewsletterContent.vue
+++ components/AppNewsletterContent.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="container">
     <form class="flex justify-center gap-x-2">
-      <AppFormField type="email" placeholder="Your Email" class="w-96" />
+      <AppFormField
+        type="email"
+        placeholder="Your Email"
+        id="email"
+        class="w-96"
+      />
       <AppButton>
         <span class="hidden sm:inline">Subscribe</span>
         <Icon name="plane" class="size-5 sm:hidden" />
