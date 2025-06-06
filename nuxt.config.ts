bfd7f78dd8f51cf057b7d3d295456d6c2784af56
Igor Babko <i.i.babko@gmail.com>
2025-06-06 10:20:12 +0300

    33. Playlists store

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/google-fonts', '@nuxt/icon'],
+  modules: ['@nuxtjs/google-fonts', '@nuxt/icon', '@pinia/nuxt'],
   css: ['~/assets/css/main.css'],
   vite: {
     plugins: [tailwindcss()],
