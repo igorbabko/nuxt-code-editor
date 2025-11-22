9a793824d89c4cff7d84e9d6f94c70cc40ab3c38
Igor Babko <i.i.babko@gmail.com>
2025-11-22 20:58:05 +0200

    45. Lesson duration

--- app/utils/getPlaylistLink.ts
+++ app/utils/getPlaylistLink.ts
@@ -1,6 +1,6 @@
 export function getPlaylistLink(playlist: Playlist, lesson?: Lesson) {
   if (!lesson) {
-    throw createError({ statusCode: 404, message: 'Lesson not found' })
+    throw404('Lesson not found')
   }
 
   return `/playlists/${slugify(playlist.title)}/lessons/${slugify(lesson.title)}`
