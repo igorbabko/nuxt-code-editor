c376f3b70af40969164c57691e7a1e75e25eb9e1
Igor Babko <i.i.babko@gmail.com>
2025-11-22 17:46:53 +0200

    44. Fetching lesson from the store

--- app/pages/playlists/[playlistSlug]/lessons/[lessonSlug].vue
+++ app/pages/playlists/[playlistSlug]/lessons/[lessonSlug].vue
@@ -1,6 +1,29 @@
+<script setup lang="ts">
+const route = useRoute()
+const playlistsStore = usePlaylistsStore()
+const lessonsStore = useLessonsStore()
+
+const playlist = playlistsStore.getPlaylistBySlug(
+  route.params.playlistSlug as string,
+)
+
+if (!playlist) {
+  throw createError({ statusCode: 404, message: 'Playlist not found' })
+}
+
+const lesson = lessonsStore.getLessonBySlug(route.params.lessonSlug as string)
+
+if (!lesson || !playlist.lessonIds.includes(lesson.id)) {
+  throw createError({ statusCode: 404, message: 'Lesson not found' })
+}
+</script>
+
 <template>
   <div class="flex flex-col gap-y-6 md:gap-y-8 lg:col-span-2">
-    <PlaylistLessonVideo />
-    <PlaylistLessonContent />
+    <PlaylistLessonVideo :title="lesson.title" />
+    <PlaylistLessonContent
+      :title="lesson.title"
+      :description="lesson.description"
+    />
   </div>
 </template>
