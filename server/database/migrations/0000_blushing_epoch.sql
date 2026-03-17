44e90cc5c5e0cb5248a1d1af72404267454ae012
Igor Babko <i.i.babko@gmail.com>
2026-03-17 20:17:17 +0200

    chore(db): expand database schema for tags, lessons, and playlists

--- server/database/migrations/0000_blushing_epoch.sql
+++ server/database/migrations/0000_blushing_epoch.sql
@@ -5,3 +5,38 @@ CREATE TABLE `users` (
 );
 --> statement-breakpoint
 CREATE UNIQUE INDEX `users_email_unique` ON `users` (`email`);
+--> statement-breakpoint
+CREATE TABLE `tags` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE `lessons` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`title` text NOT NULL,
+	`slug` text NOT NULL,
+	`description` text NOT NULL,
+	`duration` integer NOT NULL,
+	`playlist_id` integer NOT NULL,
+	`order` integer NOT NULL,
+	FOREIGN KEY (`playlist_id`) REFERENCES `playlists`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `lessons_slug_unique` ON `lessons` (`slug`);
+--> statement-breakpoint
+CREATE TABLE `playlists` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`title` text NOT NULL,
+	`slug` text NOT NULL,
+	`description` text NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `playlists_slug_unique` ON `playlists` (`slug`);
+--> statement-breakpoint
+CREATE TABLE `playlists_to_tags` (
+	`playlist_id` integer NOT NULL,
+	`tag_id` integer NOT NULL,
+	PRIMARY KEY(`playlist_id`, `tag_id`),
+	FOREIGN KEY (`playlist_id`) REFERENCES `playlists`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`tag_id`) REFERENCES `tags`(`id`) ON UPDATE no action ON DELETE no action
+);
--- server/database/schema.ts
+++ server/database/schema.ts
@@ -1,7 +1,55 @@
-import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
+import { integer, sqliteTable, text, primaryKey } from 'drizzle-orm/sqlite-core'
+import { relations } from 'drizzle-orm'
 
 export const users = sqliteTable('users', {
   id: integer('id').primaryKey({ autoIncrement: true }),
   email: text('email').notNull().unique(),
   password: text('password').notNull(),
 })
+
+export const tags = sqliteTable('tags', {
+  id: integer('id').primaryKey({ autoIncrement: true }),
+  name: text('name').notNull(),
+})
+
+export const lessons = sqliteTable('lessons', {
+  id: integer('id').primaryKey({ autoIncrement: true }),
+  title: text('title').notNull(),
+  slug: text('slug').notNull().unique(),
+  description: text('description').notNull(),
+  duration: integer('duration').notNull(),
+  playlistId: integer('playlist_id')
+    .notNull()
+    .references(() => playlists.id),
+  order: integer('order').notNull(),
+})
+
+export const playlists = sqliteTable('playlists', {
+  id: integer('id').primaryKey({ autoIncrement: true }),
+  title: text('title').notNull(),
+  slug: text('slug').notNull().unique(),
+  description: text('description').notNull(),
+})
+
+export const playlistsRelations = relations(playlists, ({ many }) => ({
+  playlistsToTags: many(playlistsToTags),
+  lessons: many(lessons),
+}))
+
+export const playlistsToTags = sqliteTable('playlists_to_tags', {
+  playlistId: integer('playlist_id')
+    .notNull()
+    .references(() => playlists.id),
+  tagId: integer('tag_id')
+    .notNull()
+    .references(() => tags.id),
+}, (t) => ({
+  pk: primaryKey({ columns: [t.playlistId, t.tagId] }),
+}))
+
+export const playlistsToTagsRelations = relations(playlistsToTags, ({ one }) => ({
+  tag: one(tags, {
+    fields: [playlistsToTags.tagId],
+    references: [tags.id],
+  }),
+}))
